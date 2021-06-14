4bcb3b2de16a6771e9844f31684b0078ca9bfab7
sachinsah1997 <ssk1222014@gmail.com>
2021-06-14 20:22:52 +0530

    Create Dashboard and added pictures to it

--- app/src/main/java/com/sachinsah/mathapp/MainActivity.java
+++ app/src/main/java/com/sachinsah/mathapp/MainActivity.java
@@ -2,13 +2,26 @@ package com.sachinsah.mathapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialButton continueButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        continueButton = (MaterialButton)findViewById(R.id.ma_continue);
+
+        continueButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+            startActivity(intent);
+        });
     }
 }
