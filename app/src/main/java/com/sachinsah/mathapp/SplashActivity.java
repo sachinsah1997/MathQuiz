d754b463068f7340549cb554d57c9a910d188790
sachinsah1997 <ssk1222014@gmail.com>
2021-06-14 15:03:17 +0530

    Added Splash Screen logic

--- app/src/main/java/com/sachinsah/mathapp/SplashActivity.java
+++ app/src/main/java/com/sachinsah/mathapp/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.sachinsah.mathapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,5 +12,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent i= new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(i); //start new activity
+            finish();
+        }, 3000);
     }
 }
