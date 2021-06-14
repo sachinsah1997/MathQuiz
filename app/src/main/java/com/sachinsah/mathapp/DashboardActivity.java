0ec646a98d996215cfe7fe93d2d6b4e814b1ea4d
sachinsah1997 <ssk1222014@gmail.com>
2021-06-14 21:18:35 +0530

    Stored username in sharedPreferences and set to dashboard

--- app/src/main/java/com/sachinsah/mathapp/DashboardActivity.java
+++ app/src/main/java/com/sachinsah/mathapp/DashboardActivity.java
@@ -1,12 +1,32 @@
 package com.sachinsah.mathapp;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private TextView textViewName;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        // Retrieving the value using its keys the file name
+        // must be same in both saving and retrieving the data
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+
+        // The value will be default as empty string because for
+        // the very first time when the app is opened, there is nothing to show
+        String userName = sharedPreferences.getString("name", "");
+
+        // We can then use the data
+        textViewName = findViewById(R.id.textViewName);
+        textViewName.setText(textViewName.getText().toString().trim() + " " + userName);
     }
 }
--- app/src/main/java/com/sachinsah/mathapp/MainActivity.java
+++ app/src/main/java/com/sachinsah/mathapp/MainActivity.java
@@ -3,23 +3,44 @@ package com.sachinsah.mathapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private MaterialButton continueButton;
+    private EditText editTextName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        continueButton = (MaterialButton)findViewById(R.id.ma_continue);
+        editTextName = findViewById(R.id.user_name);
 
+        //continue button
+        continueButton = findViewById(R.id.ma_continue);
         continueButton.setOnClickListener(v -> {
+
+            // Storing data into SharedPreferences
+            SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+
+            // Creating an Editor object to edit(write to the file)
+            SharedPreferences.Editor myEdit = sharedPreferences.edit();
+
+            // Storing the key and its value as the data fetched from edittext
+            myEdit.putString("name", editTextName.getText().toString().trim());
+
+            // Once the changes have been made,
+            // we need to commit to apply those changes made, otherwise, it will throw an error
+            myEdit.apply();
+
+            //Intent
             Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
             startActivity(intent);
         });
